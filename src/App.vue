6547660a47081277515adb2fca2ba4884011c7d4
Udit Sen <udisen97@gmail.com>
2021-06-13 06:32:08 +0530

    TextEditor : WelcomeNote

--- src/App.vue
+++ src/App.vue
@@ -17,16 +17,45 @@ export default {
   name: 'App',
   data() {
     return {
-      text: ''
+      text: '',
+      startText:
+        'Hi, I am Udit Sen. I am a Frontend Developer.\nYou can check out my portfolio at https://aedit.me\nThis is a fun keyboard I have developed using Vue.js.\nMy github: https://github.com/aedit --- My Linkedin: https://linkedin.com/in/aedit\nFor useful shortcuts hit CTRL + T\nHit any key to get started',
+      allowTyping: false,
+      firstStroke: true
     }
   },
   components: { KeyBoard },
+  mounted() {
+    this.initalizeStartText()
+  },
   methods: {
+    checkFirstStroke() {
+      if (this.firstStroke) {
+        this.text = ''
+        this.firstStroke = false
+      }
+    },
     handleKeyStroke(evt) {
-      this.text += evt
+      if (this.allowTyping) {
+        this.checkFirstStroke()
+        this.text += evt
+      }
     },
     handleBackspace() {
-      this.text = this.text.slice(0, -1)
+      if (this.allowTyping) {
+        this.checkFirstStroke()
+        this.text = this.text.slice(0, -1)
+      }
+    },
+    initalizeStartText() {
+      let index = 0
+      let interval = setInterval(() => {
+        this.text += this.startText[index++]
+        if (index === this.startText.length) {
+          clearInterval(interval)
+          this.allowTyping = true
+        }
+      }, 50)
     }
   }
 }
